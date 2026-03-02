3cb8744b72f8708c63b74c2a9a822a89d26e97ac
gabriel <gabrielh2256@gmail.com>
2026-03-02 00:11:58 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1/Views/ListaProduto.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1.Views;
+
+public partial class ListaProduto : ContentPage
+{
+    public ListaProduto()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiApp1/Views/NovoProduto.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1.Views;
+
+public partial class NovoProduto : ContentPage
+{
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+}
