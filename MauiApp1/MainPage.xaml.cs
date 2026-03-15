f16e35189ad13bbf43ebc1cca2efdcefe97990dd
gabriel <gabrielh2256@gmail.com>
2026-03-15 01:23:48 -0300

    a

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,25 +1,43 @@
-﻿ namespace MauiApp1
+﻿namespace MauiApp1
+{
+    using System.Collections.ObjectModel;
+
+    public partial class MainPage : ContentPage
     {
-        public partial class MainPage : ContentPage
+
+        ObservableCollection<Produto> Produtos;
+        ObservableCollection<Produto> ProdutosFiltrados;
+
+        public MainPage()
         {
-            int count = 0;
+            InitializeComponent();
 
-            public MainPage()
-            {
-                InitializeComponent();
-            }
+            Produtos = new ObservableCollection<Produto>()
+        {
+            new Produto { Nome="Mouse", Preco=50 },
+            new Produto { Nome="Teclado", Preco=120 },
+            new Produto { Nome="Monitor", Preco=900 },
+            new Produto { Nome="Headset", Preco=200 }
+        };
 
-            private void OnCounterClicked(object sender, EventArgs e)
-            {
-                count++;
+            ProdutosFiltrados = new ObservableCollection<Produto>(Produtos);
 
-                if (count == 1)
-                    CounterBtn.Text = $"Clicked {count} time";
-                else
-                    CounterBtn.Text = $"Clicked {count} times";
-
-                SemanticScreenReader.Announce(CounterBtn.Text);
-            }
+            ListaProdutos.ItemsSource = ProdutosFiltrados;
         }
 
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string texto = e.NewTextValue.ToLower();
+
+            ProdutosFiltrados.Clear();
+
+            foreach (var produto in Produtos)
+            {
+                if (produto.Nome.ToLower().Contains(texto))
+                {
+                    ProdutosFiltrados.Add(produto);
+                }
+            }
+        }
     }
+}
